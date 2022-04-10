1c856714eb4bc4563a4c00c900849f8f3fb4bee5
raghavjnvk <96766510+raghavjnvk@users.noreply.github.com>
2022-04-10 22:38:31 +0530

    MergeSortImplementation.java
    Creation of new java file MergeSortImplementation.java

---
+++ com.greatlearning.dsa.labsession2/MergeSortImplementation.java
@@ -0,0 +1,64 @@
+package com.greatlearning.dsa.labsession2;
+
+public class MergeSortImplementation {
+
+    void merge(int arr[], int left, int mid, int right) {
+        /* Find the size of two sub arrays to be merged */
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        /* Creation of temporary arrays */
+        int leftArray[] = new int[n1];
+        int rightArray[] = new int[n2];
+
+        /* Copy data to temporary arrays */
+        for (int i = 0; i < n1; ++i) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            rightArray[j] = arr[mid + 1 + j];
+        }
+        /* Merge the temporary arrays */
+
+        /* Initial indexes of first and second sub-arrays */
+        int i = 0;
+        int j = 0;
+        /* Initial index of merged sub-array */
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] >= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+        /* Copy Of remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        /* Copy of  remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    /* Main function that sorts  array [left...right ] using merge sort  */
+    void sort(int notes[], int left, int right) {
+        if (left < right) {
+            /* find the middle element */
+            int mid = (left + right) / 2;
+            /* Sort first and second values */
+            sort(notes, left, mid);
+            sort(notes, mid + 1, right);
+            /* Merge the sorted halves */
+            merge(notes, left, mid, right);
+
+        }
+    }
+}
